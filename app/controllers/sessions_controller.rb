c9e4dc060cfcbddb71e95b443a3c6121f16bb186
KengoNagata <majorboy47@gmail.com>
2020-02-11 05:23:19 +0900

    アクセス制限、ログアウト機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+  
   def new
   end
   
   def create
-    user = User.find_by(email: session_params[:email]
+    user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id 
@@ -11,6 +13,11 @@ class SessionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
   end
 
   private
