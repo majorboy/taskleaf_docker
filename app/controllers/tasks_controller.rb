c788ae16aef85fd27eeb325b585eb2350985d40c
KengoNagata <majorboy47@gmail.com>
2020-02-11 05:33:04 +0900

    tasks_controller→アソシエーション利用で書き換え

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show,:edit, :update, :destroy]
+
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,23 +13,20 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def create
-    task = Task.new(task_params)
+    task = current_user.tasks.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   def update
-    task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
@@ -36,5 +34,9 @@ class TasksController < ApplicationController
   private
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_tasks
+    current_user.tasks.find(params[:id])
   end
 end
