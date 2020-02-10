0f9c07b197910df01b146b47eafe4b623126856a
KengoNagata <majorboy47@gmail.com>
2020-02-11 05:55:58 +0900

    ユーザー管理機能にアクセス制限を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :login_required
-
+  
   private
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -10,4 +10,6 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to login_path unless current_user
   end
+
+  
 end
